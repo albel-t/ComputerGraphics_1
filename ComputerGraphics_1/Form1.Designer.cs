4947366d9263d7667c1dfcce0e6625ca275e6a10
albel-t <belovalexnder1@gmail.com>
2026-02-16 11:24:50 +0300

    Добавьте файлы проекта.

---
+++ ComputerGraphics_1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ComputerGraphics_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRebuildWindow = new System.Windows.Forms.Button();
+            this.Labelx1 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.Labely1 = new System.Windows.Forms.Label();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.Labelx2 = new System.Windows.Forms.Label();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.Labely2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelScale = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRebuildWindow
+            // 
+            this.buttonRebuildWindow.Location = new System.Drawing.Point(547, 415);
+            this.buttonRebuildWindow.Name = "buttonRebuildWindow";
+            this.buttonRebuildWindow.Size = new System.Drawing.Size(119, 23);
+            this.buttonRebuildWindow.TabIndex = 0;
+            this.buttonRebuildWindow.Text = "Rebuild Window";
+            this.buttonRebuildWindow.UseVisualStyleBackColor = true;
+            this.buttonRebuildWindow.Click += new System.EventHandler(this.buttonRebuildWindow_Click);
+            // 
+            // Labelx1
+            // 
+            this.Labelx1.AutoSize = true;
+            this.Labelx1.Location = new System.Drawing.Point(544, 132);
+            this.Labelx1.Name = "Labelx1";
+            this.Labelx1.Size = new System.Drawing.Size(20, 13);
+            this.Labelx1.TabIndex = 1;
+            this.Labelx1.Text = "X1";
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(585, 129);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(46, 20);
+            this.textBoxX1.TabIndex = 2;
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Location = new System.Drawing.Point(585, 155);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(46, 20);
+            this.textBoxY1.TabIndex = 4;
+            // 
+            // Labely1
+            // 
+            this.Labely1.AutoSize = true;
+            this.Labely1.Location = new System.Drawing.Point(544, 158);
+            this.Labely1.Name = "Labely1";
+            this.Labely1.Size = new System.Drawing.Size(20, 13);
+            this.Labely1.TabIndex = 3;
+            this.Labely1.Text = "Y1";
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(696, 129);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(46, 20);
+            this.textBoxX2.TabIndex = 8;
+            // 
+            // Labelx2
+            // 
+            this.Labelx2.AutoSize = true;
+            this.Labelx2.Location = new System.Drawing.Point(655, 132);
+            this.Labelx2.Name = "Labelx2";
+            this.Labelx2.Size = new System.Drawing.Size(20, 13);
+            this.Labelx2.TabIndex = 7;
+            this.Labelx2.Text = "X2";
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Location = new System.Drawing.Point(696, 155);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(46, 20);
+            this.textBoxY2.TabIndex = 6;
+            // 
+            // Labely2
+            // 
+            this.Labely2.AutoSize = true;
+            this.Labely2.Location = new System.Drawing.Point(655, 158);
+            this.Labely2.Name = "Labely2";
+            this.Labely2.Size = new System.Drawing.Size(20, 13);
+            this.Labely2.TabIndex = 5;
+            this.Labely2.Text = "Y2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "dot",
+            "line",
+            "eraser"});
+            this.comboBox1.Location = new System.Drawing.Point(547, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(84, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Bresenham\'s line algorithm",
+            "Xiaolin Wu\'s line algorithm"});
+            this.comboBox2.Location = new System.Drawing.Point(658, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(84, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(499, 426);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(696, 415);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(92, 23);
+            this.buttonApply.TabIndex = 12;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(585, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // labelScale
+            // 
+            this.labelScale.AutoSize = true;
+            this.labelScale.Location = new System.Drawing.Point(544, 236);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(34, 13);
+            this.labelScale.TabIndex = 13;
+            this.labelScale.Text = "Scale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelScale);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.Labelx2);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.Labely2);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.Labely1);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.Labelx1);
+            this.Controls.Add(this.buttonRebuildWindow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRebuildWindow;
+        private System.Windows.Forms.Label Labelx1;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.Label Labely1;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label Labelx2;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.Label Labely2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelScale;
+    }
+}
+
---
+++ ComputerGraphics_1/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ComputerGraphics_1
+{
+
+    public partial class Form1 : Form
+    {
+        // Добавьте эти поля в начало класса
+        private Bitmap bitmapPrint;
+        private Bitmap bitmapDebug;
+        private Pen pen;
+        private Color debugColor = Color.Green;
+        private Color printColor = Color.Black;
+        private int pointSize = 5;
+        private Timer timer;
+        private int scale;
+        //private ;
+        private List<Coord2D> dots;
+        private MouseClickState mouse;
+        string selectedTool;
+        string methodSolution;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDrawing();
+            InitializeTimer();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Этот метод можно оставить пустым, если он не нужен
+            // Или перенести сюда код инициализации, если хотите
+        }
+        // Добавьте этот новый метод для инициализации рисования
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 33;
+            timer.Tick += OneTimerTick;
+            timer.Start();
+        }
+        private void InitializeDrawing()
+        {
+            textBox1.Text = $"{10}";
+            scale = int.Parse(textBox1.Text);
+            selectedTool = comboBox1.SelectedItem?.ToString() ?? "dot";
+            methodSolution = comboBox2.SelectedItem?.ToString() ?? "Bresenham's line algorithm";
+            // Создание Bitmap с размерами PictureBox
+            bitmapPrint = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bitmapDebug = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            using (Graphics g = Graphics.FromImage(bitmapPrint))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = bitmapPrint;
+
+            // Настройка пера
+            pen = new Pen(printColor, pointSize);
+
+            // Подписка на событие мыши (вместо Click используем MouseClick для координат)
+            pictureBox1.MouseClick += PictureBox1_MouseClick;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            mouse = new MouseClickState();
+
+            // Настройка ComboBox, если нужно
+            comboBox1.SelectedIndex = 0; // Выбираем "dot" по умолчанию
+        }
+        private void SetDot(Graphics g, int x, int y)
+        {
+
+            using (SolidBrush brush = new SolidBrush(printColor))
+            {
+                g.FillRectangle(brush, (x/ scale)* scale - scale / 2, (y/ scale)* scale - scale / 2, scale, scale);
+            }
+            
+        }
+        private void SetPixel(Graphics g, int x, int y, double b = 1)
+        {
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(255-(int)(b*255), 255-(int)(b*255), 255-(int)(b*255))))
+
+            {
+                g.FillRectangle(brush, x  * scale - scale / 2, y * scale - scale / 2, scale, scale);
+            }
+
+        }
+        public void DrawLineBresenham(Graphics g, int x1, int y1, int x2, int y2)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int sx = x1 < x2 ? 1 : -1;
+            int sy = y1 < y2 ? 1 : -1;
+            int err = dx - dy;
+    
+            while (true)
+            {
+                // Рисуем текущий пиксель
+                SetPixel(g, x1, y1);
+        
+                // Проверяем, достигли ли конца линии
+                if (x1 == x2 && y1 == y2) break;
+        
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+
+            }
+        }
+        public void DrawLineWu(Graphics g, int x1, int y1, int x2, int y2)
+        {
+            bool steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+
+            // Транспонируем, если линия крутая
+            if (steep)
+            {
+                Swap(ref x1, ref y1);
+                Swap(ref x2, ref y2);
+            }
+
+            // Убеждаемся, что x1 < x2
+            if (x1 > x2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+
+            // Рисуем первую точку
+            DrawPointWu(g, steep, x1, y1, 1.0);
+            DrawPointWu(g, steep, x2, y2, 1.0);
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            double gradient = dx == 0 ? 1.0 : (double)dy / dx;
+
+            // Первый y-пересечение
+            double y = y1 + gradient;
+
+            for (int x = x1 + 1; x < x2; x++)
+            {
+                // Рисуем два пикселя с разной яркостью
+                DrawPointWu(g, steep, x, (int)y, 1 - (y - Math.Floor(y)));
+                DrawPointWu(g, steep, x, (int)y + 1, y - Math.Floor(y));
+
+                y += gradient;
+            }
+        }
+
+        private void DrawPointWu(Graphics g, bool steep, int x, int y, double brightness)
+        {
+            if (brightness <= 0 || brightness > 1) return;
+
+            if (steep)
+            {
+                SetPixel(g, y, x, brightness);
+            }
+            else
+            {
+                SetPixel(g, x, y, brightness);
+            }
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        // Добавьте этот обработчик для рисования точек
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                
+
+                    mouse.click(e, scale, selectedTool);
+                    switch(mouse.currentSelect())
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                        using (Graphics g = Graphics.FromImage(bitmapPrint))
+                        {
+                            textBoxX1.Text = $"{mouse.firstClick.Get().x}";
+                            textBoxY1.Text = $"{mouse.firstClick.Get().y}";
+                            switch (selectedTool)
+                            {
+                                case "dot":
+                                case "line":
+
+                                    // Рисуем точку
+                                    SetDot(g, mouse.firstClick.x, mouse.firstClick.y);
+                                    break;
+
+                                case "eraser":
+                                    // Ластик (рисуем белым)
+                                    using (SolidBrush brush = new SolidBrush(Color.White))
+                                    {
+                                        g.FillRectangle(brush, e.X - pointSize, e.Y - pointSize, pointSize * 2, pointSize * 2);
+                                    }
+                                    break;
+
+                                    // Здесь потом добавите другие инструменты
+                            }
+                        }
+                            break;
+                        case 2:
+                        using (Graphics g = Graphics.FromImage(bitmapPrint))
+                        {
+                            textBoxX2.Text = $"{mouse.secondClick.Get().x}";
+                            textBoxY2.Text = $"{mouse.secondClick.Get().y}";
+                            switch (selectedTool)
+                            {
+                                case "line":
+                                    switch (methodSolution)
+                                    {
+                                        case "Bresenham's line algorithm":
+                                            DrawLineBresenham(g,
+                                                mouse.firstClick.Get().x, mouse.firstClick.Get().y,
+                                                mouse.secondClick.Get().x, mouse.secondClick.Get().y);
+                                            break;
+
+                                        case "Xiaolin Wu's line algorithm":
+                                            DrawLineWu(g,
+                                                mouse.firstClick.Get().x, mouse.firstClick.Get().y,
+                                                mouse.secondClick.Get().x, mouse.secondClick.Get().y);
+                                            break;
+
+                                    }
+
+                                    break;
+                            }
+                        }
+                        break; 
+                    }
+
+                  
+
+                
+
+                pictureBox1.Refresh();
+            }
+        }
+        private void OneTimerTick(object sender, EventArgs e)
+        {
+
+        }
+        //private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Coord2D tmpDot = new Coord2D(e, scale);
+            switch (mouse.nextSelect(selectedTool))
+            {
+                
+                case 1:
+                    textBoxX1.Text = $"{tmpDot.Get().x}";
+                    textBoxY1.Text = $"{tmpDot.Get().y}";
+
+                    break; 
+                case 2:
+
+                    textBoxX2.Text = $"{tmpDot.Get().x}";
+                    textBoxY2.Text = $"{tmpDot.Get().y}";
+                    break;
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Здесь можно менять настройки в зависимости от выбранного инструмента
+            
+
+            switch (selectedTool)
+            {
+                case "eraser":
+                    // Для ластика можно увеличить размер
+                    pointSize = 10;
+                    break;
+                default:
+                    pointSize = 5;
+                    break;
+            }
+        }
+
+        public partial class MouseClickState
+        {
+            public Coord2D firstClick;
+            public Coord2D secondClick;
+            private int clickState;
+            public MouseClickState()
+            {
+                reset();
+            }
+            public void click(MouseEventArgs e, int scale_, string brush = "dot")
+            {
+                switch (brush)
+                {
+                    case "eraser":
+                    case "dot":
+
+                        clickState = (clickState + 1) % 2;
+                        break;
+                    case "line":
+                        clickState = (clickState + 1) % 3;
+                        break;
+                }
+                switch (clickState)
+                {
+                    case 1:
+                        firstClick.Withdraw(e, scale_);
+                        break;
+                    case 2:
+                        secondClick.Withdraw(e, scale_);
+                        break;
+                    case 0:
+                        firstClick = new Coord2D(0, 0, 0);
+                        secondClick = new Coord2D(0, 0, 0);
+                        break;
+                }
+            }
+            public void reset()
+            {
+                clickState = 0;
+                firstClick = new Coord2D(0, 0, 0);
+                secondClick = new Coord2D(0, 0, 0);
+            }
+            public int currentSelect()
+            {
+                return clickState;
+            }
+            public int nextSelect(string brush = "dot")
+            {
+                switch (brush)
+                {
+                    case "eraser":
+                    case "dot":
+                        return ((clickState + 1) % 2);
+                    case "line":
+                        return ((clickState + 1) % 3);
+                }
+                return 0;
+            }
+        }
+        public partial class Coord2D
+        {
+            public int x;
+            public int y;
+            private int scale;
+            public Coord2D(int x_, int y_, int scale_)
+            {
+                x = x_;
+                y = y_;
+                scale = scale_;
+            }
+            public Coord2D(MouseEventArgs e, int scale_)
+            {
+                Withdraw(e, scale_);
+            }
+            public void Withdraw(MouseEventArgs e, int scale_)
+            {
+                x = e.X;
+                y = e.Y;
+                scale = scale_;
+            }
+            public Coord2D Get()
+            {
+                return new Coord2D(x / scale, y / scale, 1);
+            }
+            public bool exists()
+            {
+                return scale == 0;
+            }
+        }
+
+        private void buttonRebuildWindow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            scale = int.Parse(textBox1.Text);
+            selectedTool = comboBox1.SelectedItem?.ToString() ?? "dot";
+            methodSolution = comboBox2.SelectedItem?.ToString() ?? "Bresenham's line algorithm";
+        }
+    }
+}
